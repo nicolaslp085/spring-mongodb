f75ec1460f40c4e4d7ec7d0c3db62420294b53c9
juan.garcia <jupagaru@gmail.com>
2021-10-20 23:16:38 -0500

    Se modifica la prueba obteniendo el puerto con la notación LocalServerPort

--- src/test/java/com/jpgarcia/arquitectura/springMongoDB/HttpRequestTest.java
+++ src/test/java/com/jpgarcia/arquitectura/springMongoDB/HttpRequestTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class HttpRequestTest {
@@ -14,9 +15,12 @@ class HttpRequestTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @LocalServerPort
+    private int port;
+    
     @Test
     public void testSaludoPorDefecto() throws Exception {
-        assertThat(this.restTemplate.getForObject("http://localhost:8080/api/hola",
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/hola",
                 String.class)).contains("Hola Mundo!");
     }
 
